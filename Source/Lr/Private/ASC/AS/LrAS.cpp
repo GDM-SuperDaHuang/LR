0e7ba445f78737ef8b0078ca3ff08a7432a56884
be_gdm <MengYen333@outlook.com>
2026-02-05 17:46:22 +0800

    1

--- Source/Lr/Private/ASC/AS/LrAS.cpp
+++ Source/Lr/Private/ASC/AS/LrAS.cpp
@@ -50,8 +50,11 @@ void ULrAS::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifet
 	DOREPLIFETIME_CONDITION_NOTIFY(ULrAS, MaxEndurance, COND_None, REPNOTIFY_Always);
 }
 
-/* ---------- 属性即将被修改 ---------- */
-//执行时机是属性值即将被修改但尚未实际更新到 FGameplayAttributeData 之前
+/**
+ * 属性即将被修改
+ * 执行时机是属性值即将被修改但尚未实际更新到 FGameplayAttributeData 之前
+ * NewValue : 即将写入 BaseValue 或 CurrentValue 前的目标值。
+ */
 void ULrAS::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
 {
 	Super::PreAttributeChange(Attribute, NewValue);
@@ -73,14 +76,26 @@ void ULrAS::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewVa
 	}
 }
 
+/**
+ * 
+ * 伤害结算 / 生命扣减 / 副作用
+ * 
+ * GE 执行完成后（BaseValue 已落地）
+ * PostGameplayEffectExecute 可有可无，这里修改的是连带属性，
+ * 比如有一个施加中毒GE，需要ui，特效等显示，这个不适合直接修改Hp，需要中间属性Damage的属性进行了变化，这时可以对HP属性修改。
+ * 
+ */
 /* ---------- GE 执行完毕，本次 GE 对属性的数值修改已经“落袋”的回调 ---------- */
 void ULrAS::PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data)
 {
 	Super::PostGameplayEffectExecute(Data);
 }
 
-/*
-/* ---------- GE 执行完毕，本次 GE 对属性的数值修改已经“落袋”的回调 ---------- */
+/**
+ * GE 执行完毕，本次 GE 对属性的数值修改已经“落袋”的回调
+ * UI / 状态监听 / 触发事件 /动画 / 特效触发
+ * 禁止修改否则，无限递归
+ */
 void ULrAS::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
 {
 	Super::PostAttributeChange(Attribute, OldValue, NewValue);
