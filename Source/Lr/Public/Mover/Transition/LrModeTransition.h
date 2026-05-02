9eb67db1afbac4ba396b99048814504053471728
gdm <1875525594@qq.com>
2026-05-03 00:51:02 +0800

    空中移动模式

---
+++ Source/Lr/Public/Mover/Transition/LrModeTransition.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MovementModeTransition.h"
+#include "LrModeTransition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LR_API ULrModeTransition : public UBaseMovementModeTransition
+{
+	GENERATED_BODY()
+};
